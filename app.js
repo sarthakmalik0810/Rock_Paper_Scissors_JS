a59f127ead9b90135c9d808cf8be7fd3fa1172cd
Sarthak Malik <sarthakmalik0810@gmail.com>
2020-10-03 03:07:47 +0530

    more changes to code using bind() function and anonymous functions

--- app.js
+++ app.js
@@ -20,21 +20,74 @@ let gameIsRunning = false;
 // }
 // person.greet();
 
-const getPlayerChoice = function () {
+const getPlayerChoice = () => {
     const selection = prompt(
         `${ROCK}, ${PAPER} OR ${SCISSORS}?`,
         ''
     ).toUpperCase();
     if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
         alert(`Invalid choice, we chose ${DEFAULT_CHOICE} for you!`);
-        return DEFAULT_CHOICE;
+        return;
     }
     return selection;
 };
 
-const add = (a, b) => a + b;
+const combine = (resultHandler, operation, ...numbers) => {
+    const validateNumber = (number) => {
+        return isNaN(number) ? 0 : number;
+    };
 
-const getWinner = (cChoice, pChoice) =>
+    let sum = 0;
+    for (const num of numbers) {
+        if (operation === 'ADD') {
+            sum += validateNumber(num);
+        } else {
+            sum -= validateNumber(num);
+        }
+    }
+    resultHandler(sum);
+};
+
+// const subtractUp = (resultHandler, ...numbers) => {
+//     let sum = 0;
+//     for (const num of numbers) {
+//         sum += num;
+//     }
+//     resultHandler(sum);
+// };
+const showResult = (messageText, result) => {
+    alert(messageText + ' ' + result);
+};
+
+combine(
+    showResult.bind(this, 'The result after adding all numbers is: '),
+    'ADD',
+    1,
+    5,
+    'fsda',
+    -3,
+    6,
+    10
+);
+combine(
+    showResult.bind(this, 'The result after adding all numbers is: '),
+    'ADD',
+    1,
+    5,
+    2,
+    6,
+    80
+);
+combine(
+    showResult.bind(this, 'The result after subtracting all numbers is: '),
+    'SUBTRACT',
+    1,
+    2,
+    5,
+    8
+);
+
+const getWinner = (cChoice, pChoice = DEFAULT_CHOICE) =>
     cChoice === pChoice
         ? RESULT_DRAW
         : (cChoice === ROCK && pChoice === PAPER) ||
@@ -66,7 +119,7 @@ const getComputerChoice = function () {
     }
 };
 
-startGameBtn.addEventListener('click', function startGame() {
+startGameBtn.addEventListener('click', () => {
     if (gameIsRunning) {
         return;
     }
@@ -74,6 +127,23 @@ startGameBtn.addEventListener('click', function startGame() {
     console.log('Game is starting');
     const playerSelection = getPlayerChoice();
     const computerChoice = getComputerChoice();
-    const winner = getWinner(computerChoice, playerSelection);
-    console.log(winner);
+    let winner;
+    if (playerSelection) {
+        winner = getWinner(computerChoice, playerSelection);
+    } else {
+        winner = getWinner(computerChoice);
+    }
+    // console.log(winner);
+    let message = `You picked ${
+        playerSelection ? playerSelection : DEFAULT_CHOICE
+    } and computer picked ${computerChoice}, therefore you `;
+    if (winner === RESULT_DRAW) {
+        message = message + 'had a draw.';
+    } else if (winner === RESULT_PLAYER_WINS) {
+        message = message + 'won.';
+    } else {
+        message = message + 'lost.';
+    }
+    alert(message);
+    gameIsRunning = false;
 });
